ae5e9b4fb718a17f3cbdb4c86b796e4b69ef8b3d
rybunial <rybunial@gmail.com>
2021-06-05 15:51:22 +0200

    Gra Rusty Old wersja 1.0

---
+++ rustyold.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace rustyold
+{
+    partial class rustyold
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblGold = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblexp = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbllevel = new System.Windows.Forms.Label();
+            this.btngora = new System.Windows.Forms.Button();
+            this.rtbtext = new System.Windows.Forms.RichTextBox();
+            this.btnlewo = new System.Windows.Forms.Button();
+            this.btndol = new System.Windows.Forms.Button();
+            this.btnprawo = new System.Windows.Forms.Button();
+            this.postac = new System.Windows.Forms.Label();
+            this.rtbworld = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gold:";
+            // 
+            // lblGold
+            // 
+            this.lblGold.AutoSize = true;
+            this.lblGold.Location = new System.Drawing.Point(60, 15);
+            this.lblGold.Name = "lblGold";
+            this.lblGold.Size = new System.Drawing.Size(35, 13);
+            this.lblGold.TabIndex = 2;
+            this.lblGold.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Exp:";
+            // 
+            // lblexp
+            // 
+            this.lblexp.AutoSize = true;
+            this.lblexp.Location = new System.Drawing.Point(160, 15);
+            this.lblexp.Name = "lblexp";
+            this.lblexp.Size = new System.Drawing.Size(35, 13);
+            this.lblexp.TabIndex = 4;
+            this.lblexp.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Level:";
+            // 
+            // lbllevel
+            // 
+            this.lbllevel.AutoSize = true;
+            this.lbllevel.Location = new System.Drawing.Point(270, 15);
+            this.lbllevel.Name = "lbllevel";
+            this.lbllevel.Size = new System.Drawing.Size(35, 13);
+            this.lbllevel.TabIndex = 6;
+            this.lbllevel.Text = "label4";
+            // 
+            // btngora
+            // 
+            this.btngora.Location = new System.Drawing.Point(693, 393);
+            this.btngora.Name = "btngora";
+            this.btngora.Size = new System.Drawing.Size(75, 23);
+            this.btngora.TabIndex = 7;
+            this.btngora.Text = "Góra";
+            this.btngora.UseVisualStyleBackColor = true;
+            this.btngora.Click += new System.EventHandler(this.btngora_Click);
+            // 
+            // rtbtext
+            // 
+            this.rtbtext.Location = new System.Drawing.Point(10, 390);
+            this.rtbtext.Name = "rtbtext";
+            this.rtbtext.Size = new System.Drawing.Size(400, 50);
+            this.rtbtext.TabIndex = 8;
+            this.rtbtext.Text = "";
+            // 
+            // btnlewo
+            // 
+            this.btnlewo.Location = new System.Drawing.Point(613, 420);
+            this.btnlewo.Name = "btnlewo";
+            this.btnlewo.Size = new System.Drawing.Size(75, 23);
+            this.btnlewo.TabIndex = 9;
+            this.btnlewo.Text = "Lewo";
+            this.btnlewo.UseVisualStyleBackColor = true;
+            this.btnlewo.Click += new System.EventHandler(this.btnlewo_Click);
+            // 
+            // btndol
+            // 
+            this.btndol.Location = new System.Drawing.Point(695, 420);
+            this.btndol.Name = "btndol";
+            this.btndol.Size = new System.Drawing.Size(75, 23);
+            this.btndol.TabIndex = 10;
+            this.btndol.Text = "Dół";
+            this.btndol.UseVisualStyleBackColor = true;
+            this.btndol.Click += new System.EventHandler(this.btndol_Click);
+            // 
+            // btnprawo
+            // 
+            this.btnprawo.Location = new System.Drawing.Point(775, 420);
+            this.btnprawo.Name = "btnprawo";
+            this.btnprawo.Size = new System.Drawing.Size(75, 23);
+            this.btnprawo.TabIndex = 11;
+            this.btnprawo.Text = "Prawo";
+            this.btnprawo.UseVisualStyleBackColor = true;
+            this.btnprawo.Click += new System.EventHandler(this.btnprawo_Click);
+            // 
+            // postac
+            // 
+            this.postac.AutoSize = true;
+            this.postac.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.postac.ForeColor = System.Drawing.Color.Snow;
+            this.postac.Location = new System.Drawing.Point(18, 48);
+            this.postac.Name = "postac";
+            this.postac.Size = new System.Drawing.Size(19, 13);
+            this.postac.TabIndex = 12;
+            this.postac.Text = "<>";
+            // 
+            // rtbworld
+            // 
+            this.rtbworld.Location = new System.Drawing.Point(12, 48);
+            this.rtbworld.Name = "rtbworld";
+            this.rtbworld.Size = new System.Drawing.Size(860, 336);
+            this.rtbworld.TabIndex = 13;
+            this.rtbworld.Text = "";
+            // 
+            // rustyold
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.postac);
+            this.Controls.Add(this.btnprawo);
+            this.Controls.Add(this.btndol);
+            this.Controls.Add(this.btnlewo);
+            this.Controls.Add(this.rtbtext);
+            this.Controls.Add(this.btngora);
+            this.Controls.Add(this.lbllevel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblexp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblGold);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbworld);
+            this.Name = "rustyold";
+            this.Text = "MyGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGold;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblexp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbllevel;
+        private System.Windows.Forms.Button btngora;
+        private System.Windows.Forms.RichTextBox rtbtext;
+        private System.Windows.Forms.Button btnlewo;
+        private System.Windows.Forms.Button btndol;
+        private System.Windows.Forms.Button btnprawo;
+        private System.Windows.Forms.Label postac;
+        private System.Windows.Forms.RichTextBox rtbworld;
+    }
+}
+
---
+++ rustyold.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using engine;
+
+namespace rustyold
+{
+    public partial class rustyold : Form
+    {
+        public player _player;
+        public maps _maps;
+        public int postacx = 100;
+        public int postacy = 100;
+        public void engine(int pozycjax, int pozycjay)
+        {
+            rtbtext.Text =pozycjax.ToString() + ' ' + pozycjay.ToString();
+            if (pozycjax == 300 && pozycjay == 100)
+            { 
+                rtbtext.Text = "Dotarles do zamku";
+            }
+        }
+        public rustyold()
+        {
+            InitializeComponent();
+
+            _player = new player(0, 0, 0);
+            
+            _player.Inventory.Add(new InventoryItem("Text"));
+            _maps = new maps();
+            //  rtbworld.Visible = false;
+            dgvworld.SendToBack();
+            int ilo = 0;
+
+     
+            foreach (wlr wlrs in _maps.Wlrs)
+            {
+                int pozx0 = 50;
+                int pozy0 = 30;
+                for (int pozy = 1; pozy < 30; pozy++)
+                {
+                    pozy0 = pozy0 + 10;
+                    for (int pozx = 1; pozx < 44; pozx++)
+                    {
+                        if (pozx == 1)
+                        {
+                            pozx0 = 21;
+                        }
+                        else
+                        {
+                            pozx0 = pozx0 + 19;
+                        }
+                        // Wlrs.Add(new wlr(pozx0, pozy0, pozycja[pozy, pozx].ToString()));
+
+                        rtbworld.Text = _maps.Wlrs[1].ToString();
+                        
+                        String nazwa = pozx0.ToString() + ' ' + pozy0.ToString() + ' ' + pozycja[pozy, pozx].ToString();
+                        Console.WriteLine(nazwa);
+
+                    }
+                    pozx0 = 21;
+                }
+                /*  ilo++;
+
+                   var lab = new Label
+                   {
+
+                       Name = "label" + ilo,
+                       Size = new Size(20, 20),
+                       ForeColor = Color.White,
+                       BackColor = Color.DarkOliveGreen,
+                       Location = new Point(wlrs.Pozycjax, wlrs.Pozycjay),
+
+                        Text = wlrs.Obiekt,
+
+
+                   };
+
+                   this.Controls.Add(lab);*//
+             //   dgvworld. = wlrs.Obiekt.ToString();
+               // lab.BringToFront();
+                
+                rtbtext.Text = ilo.ToString() + '-' + wlrs.Pozycjax.ToString() + '-' + wlrs.Pozycjay.ToString();
+            }
+            lblexp.Text = _player.Exp.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lbllevel.Text = _player.Level.ToString();
+
+           
+           
+        }
+       
+        private void btngora_Click(object sender, EventArgs e)
+        {
+            postacy = postacy - 10;
+            postac.Location = new Point(postacx, postacy);
+            engine(postac.Location.X, postac.Location.Y);
+
+        }
+
+        private void btndol_Click(object sender, EventArgs e)
+        {
+            postacy = postacy + 10;
+            postac.Location = new Point(postacx, postacy);
+            engine(postac.Location.X, postac.Location.Y);
+        }
+
+        private void btnlewo_Click(object sender, EventArgs e)
+        {
+            postacx = postacx - 10;
+            postac.Location = new Point(postacx, postacy);
+            engine(postac.Location.X, postac.Location.Y);
+        }
+
+        private void btnprawo_Click(object sender, EventArgs e)
+        {
+            postacx = postacx + 10;
+            postac.Location = new Point(postacx, postacy);
+            engine(postac.Location.X, postac.Location.Y);
+        }
+    }
+}
